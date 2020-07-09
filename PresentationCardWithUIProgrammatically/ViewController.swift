206782dcbbe56b48f410e6b7d9ea4832b0c16eb9
Ezequiel <ezequiel3434@gmail.com>
2020-07-09 09:05:10 -0300

    building, load and constraints of the image

--- PresentationCardWithUIProgrammatically/ViewController.swift
+++ PresentationCardWithUIProgrammatically/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imagenDePerfil = UIImageView()
+    var constraintsToApply = Array<NSLayoutConstraint>()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buildAndLoadImageWithAutoLayout(imageName: "ezequiel")
+        self.view.layoutIfNeeded()
+        
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NSLayoutConstraint.activate(constraintsToApply)
+    }
+    
+    
+    func buildAndLoadImageWithAutoLayout(imageName: String){
+        imagenDePerfil.translatesAutoresizingMaskIntoConstraints = false
+        imagenDePerfil.image = UIImage(named: imageName)
+        imagenDePerfil.contentMode = UIImageView.ContentMode.scaleAspectFill
+        imagenDePerfil.clipsToBounds = true
+        self.view.addSubview(imagenDePerfil)
+        let centerXConstraint = imagenDePerfil.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+        let centerYConstraint = imagenDePerfil.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
+        
+        let widthConstraint = imagenDePerfil.widthAnchor.constraint(equalToConstant: 175)
+        let heightConstraint = imagenDePerfil.heightAnchor.constraint(equalToConstant: 175)
+        
+        imagenDePerfil.layer.cornerRadius = widthConstraint.constant / 2.0
+        
+        constraintsToApply.append(centerXConstraint)
+        constraintsToApply.append(centerYConstraint)
+        constraintsToApply.append(widthConstraint)
+        constraintsToApply.append(heightConstraint)
     }
 
 
